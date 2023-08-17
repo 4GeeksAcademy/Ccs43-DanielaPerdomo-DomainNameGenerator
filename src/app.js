1bdc7c18b35a6a88cc8bf7e0907c9fe6ef4df2df
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-08-17 15:42:18 +0000

    Creacion de la funcion con for anidados para la generacion de combinaciones de nombres de dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+console.log("Hello Rigo from the console!");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const ext = [".com", ".net", ".es", ".gov", ".ve"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainNameGenerator(pronouns, adjs, nouns, exts) {
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let ext of exts) {
+          console.log(pronoun + adj + noun + ext);
+        }
+      }
+    }
+  } 
+}
+
+domainNameGenerator(pronoun, adj, noun, ext);
